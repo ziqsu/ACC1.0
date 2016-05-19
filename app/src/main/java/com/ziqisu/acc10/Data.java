9488cc68f8774baa78c3b8a13cea78def818318e
ziqsu <ziqsu43@gmail.com>
2016-05-19 17:38:56 -0500

    Update Data.java

--- app/src/main/java/com/ziqisu/acc10/Data.java
+++ app/src/main/java/com/ziqisu/acc10/Data.java
@@ -3,11 +3,14 @@ package com.ziqisu.acc10;
 /**
  * Created by ziqisu on 5/18/16.
  */
+ 
+//create class data to store accelerometer data, time and activity type
 public class Data {
     protected String activity;
     protected float[] values;
     protected long time;
-
+    
+    
     public Data(long time, float[] values, String activity) {
         this.time = time;
         this.values = values;
